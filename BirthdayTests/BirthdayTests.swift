3b05d4d84f2489daa54f882748f6c8ad2f4ba4f0
Sergio Arcila <sergioarcilabusiness@gmail.com>
2025-08-19 11:25:42 -0500

    Initial Commit

---
+++ BirthdayTests/BirthdayTests.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayTests.swift
+//  BirthdayTests
+//
+//  Created by Sergio Arcila on 8/19/25.
+//
+
+import Testing
+@testable import Birthday
+
+struct BirthdayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
